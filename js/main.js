8018b6cf15e9b4bbc6c55c2ccd41cd3dd366f962
NicholasPeara92 <nicholas.peara@gmail.com>
2022-10-27 11:09:39 +0200

    Modifica eventi bottoni

--- js/main.js
+++ js/main.js
@@ -2,118 +2,59 @@
 
 // Creare un array contenente le immagini
 
-
-const items =document.querySelector(".items");
+const itemsContainer =document.querySelector(".items");
 
 const images = ["img/01.jpg", "img/02.jpg", "img/03.jpg", "img/04.jpg", "img/05.jpg",];
 
-
-const btnNext = document.querySelector(".btn-next");
-const btnPrev = document.querySelector(".btn-prev");
-
-
+// creo ciclo per aggiungere elementi nell' HTML
 for (let i = 0; i < images.length; i++) {
     const photo = images[i];
     let item = document.createElement('div');
     
     item.classList.add("item");
-    items.append(item);
     item.innerHTML =`<img src="${photo}"/>`;
     
+    itemsContainer.append(item);
     
     if ( i === 0 ) {
         item.classList.add("active");
-    }
-    
+    }    
 }
-let active = 0;
 
-let item = document.querySelectorAll(".item")[active];
+let currentImageIndex = 0;
+
+const items = document.querySelectorAll(".item");
+
+// seleziono il primo item creato in precedenza e aggiunge la classe active
+items[currentImageIndex].classList.add("active");
+
+//aggiungo evento click al pulsante next
+const btnNext = document.querySelector(".btn-next");
 
 btnNext.addEventListener('click', function() {
-    item = document.querySelectorAll(".item")[0];
-    item.classList.remove("active");
-    active += 1;
-    
-    if ( active === 1) {
-
-        item = document.querySelectorAll(".item")[1];
-        item.classList.add("active");
-        
-    } else if ( active === 2 ) {
-        item = document.querySelectorAll(".item")[1];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[2];
-        item.classList.add("active");
-
-    } else if ( active === 3 ) {
-        item = document.querySelectorAll(".item")[2];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[3];
-        item.classList.add("active");
-        
-    } else if ( active === 4 ) {
-        item = document.querySelectorAll(".item")[3];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[4];
-        item.classList.add("active");
-        
-    } else if ( active === 5 ) {
-        item = document.querySelectorAll(".item")[4];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[5];
-        item.classList.add("active");
-        
-    } else if ( active > 5 ) {
-        active === 0;
-        item = document.querySelectorAll(".item")[0];
-        item.classList.add("active");
+    items[currentImageIndex].classList.remove("active");
+    if ( currentImageIndex === images.length - 1) {
+        currentImageIndex = 0;
+    } else {
+        currentImageIndex++;
     }
-   
+    items[currentImageIndex].classList.add("active");
+       
 });
 
+// seleziono il pulsante prev
+const btnPrev = document.querySelector(".btn-prev");
+
+// aggiungo evento click al pulsante prev
 btnPrev.addEventListener('click', function() {
-    item = document.querySelectorAll(".item")[active];
-    item.classList.remove("active");
-    active -= 1;
-    
-    if ( active === 5) {
-
-        item = document.querySelectorAll(".item")[5];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[4];
-        item.classList.add("active");
-        
-    } else if ( active === 4 ) {
-        item = document.querySelectorAll(".item")[4];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[3];
-        item.classList.add("active");
-
-    } else if ( active === 3 ) {
-        item = document.querySelectorAll(".item")[3];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[2];
-        item.classList.add("active");
-        
-    } else if ( active === 2 ) {
-        item = document.querySelectorAll(".item")[2];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[1];
-        item.classList.add("active");
-        
-    } else if ( active === 1 ) {
-        item = document.querySelectorAll(".item")[1];
-        item.classList.remove("active");
-        item = document.querySelectorAll(".item")[0];
-        item.classList.add("active");
-        
-    } else if ( active < 0 ) {
-        active === 5;
-        item = document.querySelectorAll(".item")[5];
-        item.classList.add("active");
-
-
+    items[currentImageIndex].classList.remove("active"); 
+    if ( currentImageIndex === 0) {
+        currentImageIndex = images.length -1;
+    } else {
+        currentImageIndex--;
     }
-   
+    items[currentImageIndex].classList.add("active");
+     
 });
+
+
